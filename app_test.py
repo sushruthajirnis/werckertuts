97f490da05d6dbf3e74eff2dda587e3384098ac1
sushruthajirnis <sushrut.hajirnis@gmail.com>
2015-05-04 02:51:07 -0700

    response.status.code is really response.status_code you dumbass

--- app_test.py
+++ app_test.py
@@ -14,7 +14,7 @@ class CloudTestCase(unittest.TestCase):
     def test_clouds(self):
         tester=app.test_client(self)
         response = tester.get('/clouds.json', content_type='application/json')
-        self.assertEqual(response.status.code,200)
+        self.assertEqual(response.status_code,200)
         self.assertEqual(response.data,json.dumps(['Altocumulus','Altostratus','Cumulonimbus','Nimbostratus']))
 
 if __name__=='__main__':
