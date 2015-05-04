af51cb18849cb571f66fc1e5dd442d2a394959cc
sushruthajirnis <sushrut.hajirnis@gmail.com>
2015-05-04 03:00:13 -0700

    fixing app.py

--- app_test.py
+++ app_test.py
@@ -13,7 +13,6 @@ class CloudTestCase(unittest.TestCase):
         app.redis.flushdb()
     def test_clouds(self):
         tester=app.test_client(self)
-        response = tester.get('/clouds.json', content_type='application/json')
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.data,json.dumps(['Altocumulus','Altostratus','Cumulonimbus','Nimbostratus']))
 
