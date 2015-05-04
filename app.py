5f38158c7cebb154a51795bf3836b23de5d4f8c1
sushruthajirnis <sushrut.hajirnis@gmail.com>
2015-05-04 03:18:38 -0700

    redis fixes

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from flask import Response
 from flask import json
 
 app = Flask(__name__)
-app.redis = redis.StrictRedis(host=os.getenv('WERCKER_REDIS_HOST', 'localhost'),
-              port= 6379, db=0)
+#app.redis = redis.StrictRedis(host=os.getenv('WERCKER_REDIS_HOST', 'localhost'),
+#              port= 6379, db=0)
+app.redis=redis.StrictRedis(host='localhost',port='6379',db=0)
 
 @app.route("/clouds.json")
 def clouds():
       data = app.redis.lrange("clouds", 0, -1)
-        resp = Response(json.dumps(data), status=200, mimetype='application/json')
-          return resp
-
-      if __name__ == "__main__":
-            port = int(os.getenv('PORT', 5000))
-              app.run(host='0.0.0.0', port=port)
+      resp = Response(json.dumps(data), status=200, mimetype='application/json')
+      return resp
+if __name__ == "__main__":
+     port = int(os.getenv('PORT', 5000))
+     app.run(host='0.0.0.0', port=port)
 
