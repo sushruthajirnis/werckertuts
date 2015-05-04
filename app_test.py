03d0e345866b0e96a08c13fd3c2cd96b1054d76d
sushruthajirnis <sushrut.hajirnis@gmail.com>
2015-05-04 01:53:00 -0700

    initial commit

---
+++ app_test.py
@@ -0,0 +1,24 @@
+from import app
+import redis
+import os
+import unittest
+import json
+class CloudTestCase(unittest.TestCase):
+    def setUp(self):
+        app.redis.rpush('clouds','Altocumulus')
+        app.redis.rpush('clouds','Altostratus')
+        app.redis.rpush('clouds','Cumulonimbus')
+        app.redis.rpush('clouds','Nimbostratus')
+    def tearDown(self):
+        app.redis.flushdb()
+    def test_clouds(self):
+        tester=app.test_client(self)
+        response=tester.get('/clouds.json',content-type='application/json')
+        self.assertEqual(response.status.code,200)
+        self.assertEqual(response.data,json.dumps(['Altocumulus','Altostratus','Cumulonimbus','Nimbostratus']))
+
+if __name__=='__main__':
+    unittest.main()
+
+
+
